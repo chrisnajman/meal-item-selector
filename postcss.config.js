4e2c685071be1d72831e0c2bb847c65c862bef18
Chris Najman <christopher.najman@gmail.com>
2025-07-18 07:36:54 +0100

    Installed postcss-url to address the css background image path problem

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,9 @@ module.exports = {
   plugins: [
     require("postcss-import"),
     require("postcss-nesting"),
+    require("postcss-url")({
+      url: "rebase", // adjusts URLs to be relative to output CSS location
+    }),
     require("cssnano")({ preset: "default" }),
   ],
 }
